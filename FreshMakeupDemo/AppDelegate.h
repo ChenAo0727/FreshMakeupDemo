a95d1c34d01117a5b66e14ec6cf445d81e24da73
guoshencheng <648772021@qq.com>
2015-08-14 17:29:31 +0800

    Initial Commit

---
+++ FreshMakeupDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreshMakeupDemo
+//
+//  Created by guoshencheng on 8/14/15.
+//  Copyright (c) 2015 guoshencheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
