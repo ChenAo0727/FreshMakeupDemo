b97dcdc36029041b2bcabf709b1e400f20aaa9e8
chenao <jim@chenaodeMacBook-Air.local>
2015-08-31 14:45:01 +0800

    CommentView

--- FreshMakeupDemo/ViewControllers/CommentViewController/HeaderViewCell.m
+++ FreshMakeupDemo/ViewControllers/CommentViewController/HeaderViewCell.m
@@ -9,18 +9,22 @@
 #import "HeaderViewCell.h"
 #import "SelectionView.h"
 #import "UIScreen+Utility.h"
+#import "MySegmentControl.h"
 #define KScreenWidth [UIScreen mainScreen].bounds.size.width
 
 @implementation HeaderViewCell
 
 - (void)awakeFromNib {
     
+    MySegmentControl *segment = [[MySegmentControl alloc]initWithFrame:CGRectMake(90, 10, [UIScreen screenWidth] - 200, 30)];
+    [segment AddSegumentArray:[NSArray arrayWithObjects:@"所有的人",@"相近的人",nil]];
+    [self addSubview:segment];
     SelectionView *selectionView = [SelectionView create];
     [self addSubview:selectionView];
 
-    selectionView.frame = CGRectMake(40, 85, [UIScreen screenWidth] - 100, 54);
-
-
+    selectionView.frame = CGRectMake(40, 70, [UIScreen screenWidth] - 100, 25);
+    
 }
+
 
 @end
