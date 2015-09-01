ea93b8ecc3fef54805464ab26f97694c2992918e
guoshencheng <648772021@qq.com>
2015-09-01 09:52:31 +0800

    add datasource

---
+++ FreshMakeupDemo/Tools/DataSource/DetailCollectionViewDatasource.h
@@ -0,0 +1,16 @@
+//
+//  DetailCollectionViewDatasource.h
+//  FreshMakeupDemo
+//
+//  Created by guoshencheng on 9/1/15.
+//  Copyright (c) 2015 guoshencheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CHTCollectionViewWaterfallLayout.h"
+
+@interface DetailCollectionViewDatasource : NSObject<UICollectionViewDataSource, CHTCollectionViewDelegateWaterfallLayout>
+
+
+
+@end
