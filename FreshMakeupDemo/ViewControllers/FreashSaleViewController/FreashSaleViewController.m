bd8ad3fca49b6c99857ab91edff3bf423b9b304d
guoshencheng <648772021@qq.com>
2015-09-02 16:26:46 +0800

    add title for Tags

--- FreshMakeupDemo/ViewControllers/FreashSaleViewController/FreashSaleViewController.m
+++ FreshMakeupDemo/ViewControllers/FreashSaleViewController/FreashSaleViewController.m
@@ -18,6 +18,7 @@
 + (instancetype)create {
     return [[FreashSaleViewController alloc] initWithNibName:@"FreashSaleViewController" bundle:nil];
 }
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
@@ -37,22 +38,12 @@
     } else if (indexPath.row == 4) {
         return 170;
     } else {
-    return 50;
+        return 50;
     }
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
