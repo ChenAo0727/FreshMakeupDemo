02f00e1cfd5de7d1a2077a30e7b4ea6dbb77c0b9
chenao <jim@chenaodeMacBook-Air.local>
2015-09-02 11:09:27 +0800

    commentViewCell

---
+++ FreshMakeupDemo/Custom Views/Custon Cells/CommentCell/CommentViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CommentViewCell.h
+//  FreshMakeupDemo
+//
+//  Created by chenao on 15/9/2.
+//  Copyright (c) 2015年 guoshencheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentViewCell : UITableViewCell
+
+@end
---
+++ FreshMakeupDemo/Custom Views/Custon Cells/CommentCell/CommentViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CommentViewCell.m
+//  FreshMakeupDemo
+//
+//  Created by chenao on 15/9/2.
+//  Copyright (c) 2015年 guoshencheng. All rights reserved.
+//
+
+#import "CommentViewCell.h"
+
+@implementation CommentViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
