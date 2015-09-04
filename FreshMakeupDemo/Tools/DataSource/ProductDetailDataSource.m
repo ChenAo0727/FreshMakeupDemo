005c82ce99932601a37237ae5b7b7ca2762bd69f
guoshencheng <648772021@qq.com>
2015-09-04 16:38:59 +0800

    add new DetailDatasource

---
+++ FreshMakeupDemo/Tools/DataSource/ProductDetailDataSource.m
@@ -0,0 +1,34 @@
+//
+//  ProductDetailDataSource.m
+//  FreshMakeupDemo
+//
+//  Created by guoshencheng on 9/4/15.
+//  Copyright (c) 2015 guoshencheng. All rights reserved.
+//
+
+#import "ProductDetailDataSource.h"
+
+@implementation ProductDetailDataSource
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeZero;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout columnCountForSection:(NSInteger)section {
+    return 1;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 0;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+
+@end
