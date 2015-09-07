b34672d71afe2eddc14e3963afc197b150f0c5e4
zeBei <1143879052@qq.com>
2015-09-07 11:02:55 +0800

    新鲜慧  数据文件创建

---
+++ FreshMakeupDemo/Tools/FreshSaleInfomationTool.h
@@ -0,0 +1,18 @@
+//
+//  FreshSaleInfomationTool.h
+//  FreshMakeupDemo
+//
+//  Created by winchance on 15/9/7.
+//  Copyright (c) 2015年 guoshencheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FreshSaleInfomationTool : NSObject
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *price;
+@property (strong, nonatomic) NSArray *label;
+@property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) NSString *commentary;
+
+@end
---
+++ FreshMakeupDemo/Tools/FreshSaleInfomationTool.m
@@ -0,0 +1,13 @@
+//
+//  FreshSaleInfomationTool.m
+//  FreshMakeupDemo
+//
+//  Created by winchance on 15/9/7.
+//  Copyright (c) 2015年 guoshencheng. All rights reserved.
+//
+
+#import "FreshSaleInfomationTool.h"
+
+@implementation FreshSaleInfomationTool
+
+@end
