a8ef4eca03683c5d73bd94e234065a66612e0acb
guoshencheng <648772021@qq.com>
2015-09-07 19:50:37 +0800

    change height sizeing of detailbaseInfomationCell

--- FreshMakeupDemo/Custom Views/Custon Cells/DetailBaseInfomationCell/DetailBaseInfomationCell.m
+++ FreshMakeupDemo/Custom Views/Custon Cells/DetailBaseInfomationCell/DetailBaseInfomationCell.m
@@ -20,22 +20,12 @@
 }
 
 - (CGSize)sizeOfCell {
-    CGRect frame = self.detaBaseView.frame;
-    self.bounds = CGRectMake(0, 0, 320, self.frame.size.height);
+    self.bounds = CGRectMake(0, 0, [UIScreen screenWidth], self.frame.size.height);
     self.contentView.bounds = self.bounds;
     [self setNeedsLayout];
     [self layoutIfNeeded];
-    NSLayoutConstraint *tempWidthConstraint = [NSLayoutConstraint constraintWithItem:self.detaBaseSpeakLabel
-                                 attribute:NSLayoutAttributeWidth
-                                 relatedBy:NSLayoutRelationEqual
-                                    toItem:nil
-                                 attribute:NSLayoutAttributeNotAnAttribute
-                                multiplier:1.0
-                                  constant:[UIScreen screenWidth] - 20];
-    [self.detaBaseSpeakLabel addConstraint:tempWidthConstraint];
-    CGSize size = [self.detaBaseSpeakLabel systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
-    [self.detaBaseSpeakLabel removeConstraint:tempWidthConstraint];
-    return CGSizeMake([UIScreen screenWidth], frame.origin.y + size.height + 80);
+    CGRect frame = self.detaBaseSpeakLabel.frame;
+    return CGSizeMake([UIScreen screenWidth], 511 + frame.size.height);
 }
 
 - (void)updateWithTitleLabel:(NSString *)label {
