eb4106cccceeb5d010235a146955fdf2f08c4094
chenao <jim@chenaodeAir.lan>
2015-09-08 14:19:10 +0800

    finish FeelingTextCell

--- FreshMakeupDemo/ViewControllers/FeelingViewController/FeelIngViewController.m
+++ FreshMakeupDemo/ViewControllers/FeelingViewController/FeelIngViewController.m
@@ -8,6 +8,8 @@
 
 #import "FeelIngViewController.h"
 #import "FeelIngViewController+Configuration.h"
+#import "FeelingTextCell.h"
+#import "UITableView+FDTemplateLayoutCell.h"
 @interface FeelIngViewController ()
 
 @end
@@ -23,6 +25,14 @@
     [self configureViews];
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 1) {
+        
+        return [tableView fd_heightForCellWithIdentifier:FEELING_TEXT_CELL cacheByIndexPath:indexPath configuration:^(id cell) {
+            [cell updateFeelingTextLabel:@"机器人助理“度秘”的推出，就是致力于提升连接人与服务的效率。为了让“度秘”实现强大索引能力和智能交付能力，百度必须建立让3600行商户便捷接入百度服务生态的连接机制。李东旻介绍：“在度秘的背后，百度通过三种连接机制提供服务保障，他们分别是直达号、插件和店铺页。这三种机制的特色和运作模式各不相同，组合起来能覆盖不同需求的各类商家，让他们都能顺利接入百度的O2O生态中。”"];
+        }];
+
+    }
+    
     return 200;
 }
 - (IBAction)returnAction:(id)sender {
