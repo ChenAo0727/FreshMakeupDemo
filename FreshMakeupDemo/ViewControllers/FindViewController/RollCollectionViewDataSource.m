49f42ae05d0766d07ccd853afac4bdc1405b012e
zeBei <1143879052@qq.com>
2015-09-09 20:27:30 +0800

    发现 页面  两个滚动视图  图片更换

--- FreshMakeupDemo/ViewControllers/FindViewController/RollCollectionViewDataSource.m
+++ FreshMakeupDemo/ViewControllers/FindViewController/RollCollectionViewDataSource.m
@@ -11,16 +11,16 @@
 @implementation RollCollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     [self rollInitializationDate];
-    return 5;
+    return 10;
 }
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     return [self rollCollectionView:collectionView indexPath:indexPath];
 }
 //数组 初始化
 - (void)rollInitializationDate {
-    self.rollImageArray = [NSMutableArray arrayWithObjects:@"findhottopic1.png",@"findhottopic2.png",@"findhottopic3.jpg",@"findhottopic4.jpg",@"findhottopic5.jpg", nil];
-    self.rollLabelArray = [NSMutableArray arrayWithObjects:@"#DIOP的秘密",@"#香水传奇",@"#七月彩妆",@"#专业彩妆",@"#天然粉底", nil];
-    self.rollDiscussionLabelArray = [NSMutableArray arrayWithObjects:@"3477人在讨论",@"647人在讨论",@"535人在讨论",@"235人在讨论",@"3456人在讨论", nil];
+    self.rollImageArray = [NSMutableArray arrayWithObjects:@"findhottopic1.jpg",@"findhottopic2.jpg",@"findhottopic3.jpg",@"findhottopic4.jpg",@"findhottopic5.jpg",@"findhottopic6.jpg",@"findhottopic7.jpg",@"findhottopic8.png",@"findhottopic9.jpg",@"findhottopic10.jpg", nil];
+    self.rollLabelArray = [NSMutableArray arrayWithObjects:@"#美体健身",@"#美丽天使",@"#美妆探讨",@"#闺蜜交流",@"#瑜伽",@"#美甲知识",@"#健美操",@"#彩妆问答",@"#美颜小知识",@"#美肤小问答", nil];
+    self.rollDiscussionLabelArray = [NSMutableArray arrayWithObjects:@"76人在讨论",@"110人在讨论",@"60人在讨论",@"30人在讨论",@"456人在讨论",@"56人在讨论",@"136人在讨论",@"567人在讨论",@"259人在讨论",@"1564人在讨论", nil];
 }
 #pragma mark 自定义cell
 - (UICollectionViewCell *)rollCollectionView:(UICollectionView *)collectionView indexPath:(NSIndexPath *)indexPath {
